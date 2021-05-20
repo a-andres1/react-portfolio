8417b3a1834f4d621f50fbf3489889d77a80a38d
alyssa andres <alyssaandres1@gmail.com>
2021-05-19 23:20:05 -0400

    compiling page and attempted background

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Project from './components/Project';
+import Intro from './components/Intro';
+import Statement from './components/PersonalStatement';
+import Quote from './components/Quote';
+import background from './assets/images/background.jpg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div
+        className="landing-wrapper"
+        style={{ backgroundImage: `url(${background})` }}
+      ></div>
+      <Header />
+      <Intro />
+      <Statement />
+      <Project />
+      <Quote />
+      <Footer />
+    </>
   );
 }
 
